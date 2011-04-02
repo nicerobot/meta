4091eb7a6d0bb3c63761c1df461c7eb358f25941
nicerobot <git@nicerobot.org>
2011-04-01 22:52:26 -0400

    Scala parser combinator to parse Struxt.

---
+++ struxt/struxt.scala
@@ -0,0 +1,17 @@
+package org.nicerobot
+
+import scala.util.parsing.combinator._
+
+object Struxt extends JavaTokenParsers {
+ 
+  val ID = """[a-zA-Z]([a-zA-Z0-9]|_[a-zA-Z0-9])*"""r
+
+  val STR = "\".*?\""r
+ 
+  def attribute = ID ~ ( STR | ID )
+
+  def attributes = repsep(attribute, "," )
+
+  def node:Parser[Any] = ID ~ (attributes?) | ID ~ (attributes?) ~ "{" ~ (node*) ~ "}"
+
+}
