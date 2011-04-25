5f4318dc45c34d776dcb3b4fc46937c675eb3ed1
nicerobot <git@nicerobot.org>
2011-04-24 23:18:37 -0400

    build binaries. added license notices.

--- struxt/struxt.parsers/src/main/sh/stxt2xml.sh
+++ struxt/struxt.parsers/src/main/sh/stxt2xml.sh
@@ -1,6 +1,65 @@
 #!/bin/sh
-SELFD=$(cd $(dirname $0); cd -P $(dirname $(readlink $0)); pwd)
-SELF=$(basename $0 .sh)
-java -cp ${CLASSPATH}:${SELFD}/../../../target/classes/ org.nicerobot.struxt.parser.StruxtJavaParser ${1}
-echo
+# Struxt Parser
+# 
+# [The BSD License]
+# Copyright (c) 2011 Nice Robot Corporation
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+# following conditions are met:
+# 
+# Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+# disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+# disclaimer in the documentation and/or other materials provided with the distribution.
+# Neither the name of the author nor the names of its contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+SELFD=$(cd -P $(dirname $(readlink ${0} || echo ${0})); pwd)
+SELFU=$(basename $0)
+SELF=${SELFU%.*}
+NOW=$(date -u +%Y%m%dt%H%M%S)
+
+PAYLOAD=$(awk '/^__ARCHIVE__/ {print NR + 1; exit 0; }' ${0})
+PATHD=${TMPDIR:=${TMP:=$([ -w /tmp ] && echo '/tmp/' || echo './')}}${SELF}.${RANDOM}
+mkdir -p ${PATHD} 2>/dev/null && (
+  cd ${PATHD}
+  { tail -n+${PAYLOAD} ${SELFD}/${SELFU} | tar jx; } >/dev/null 2>&1
+)
+trap '{ unalias rm; unset rm; \rm -rf ${PATHD}; } >/dev/null 2>&1' 0
+java -jar ${PATHD}/struxt.jar ${@} || exit $?
 exit 0
+
+# $Id: $
+
+__NOTICE__
+#
+# ANTLR 3 License
+# 
+# [The BSD License]
+# Copyright (c) 2010 Terence Parr
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+# following conditions are met:
+# 
+# Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+# disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+# disclaimer in the documentation and/or other materials provided with the distribution.
+# Neither the name of the author nor the names of its contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+__ARCHIVE__
